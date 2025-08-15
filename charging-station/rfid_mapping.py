72d2f5fb2d820265573068a2fcfd393fa2a61622
Jesko Dammann <jesko.dammann@nitrobox.com>
2025-08-15 15:10:11 +0200

    Fix incorrect attribute name in RFID mapping (`contract_id` -> `contract_ident`)

--- charging-station/rfid_mapping.py
+++ charging-station/rfid_mapping.py
@@ -13,12 +13,12 @@ RFID_TAG_MAPPING: Dict[str, CustomerInfo] = {
     "316922528399": CustomerInfo(
         contract_id=2117046,
         debtor_ident="06cc07ed-8aa4-4111-ab75-a39ff18aba2c",
-        contract_id = "3710a184-b0bc-49cf-998b-7c15213b99ea"
+        contract_ident = "3710a184-b0bc-49cf-998b-7c15213b99ea"
     ),
     "868383524177": CustomerInfo(
         contract_id=2118389,
         debtor_ident="c4b14198-4fc1-4ef2-bc82-1f568f8bbd0a",
-        contract_id = "4e1ae42a-6df9-4f39-9afe-185672483251"
+            contract_ident = "4e1ae42a-6df9-4f39-9afe-185672483251"
     ),
     "572221031092": CustomerInfo(
         contract_id=2118579,
